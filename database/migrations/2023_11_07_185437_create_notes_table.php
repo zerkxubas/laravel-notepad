416c8e7155adc4ebf9c241e1ba1515945fb3e418
Subash Sharma <zerkxubas@gmail.com>
2023-11-08 01:49:28 +0545

    note model & migration added

---
+++ database/migrations/2023_11_07_185437_create_notes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained(); // this should be the fk from user id.
+            $table->string('title', 30)->nullable();
+            $table->longText('content')->nullable();
+            $table->boolean('status')->default(false); // true=completed && false=incompleted
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
